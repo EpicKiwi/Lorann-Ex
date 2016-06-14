e1d508e07710185f483a132aae6d0cb0e8914548
EpicKiwi <lelycandu76@gmail.com>
2016-06-14 13:55:18 +0200

    Much documentation

--- model/src/main/java/model/Sprite.java
+++ model/src/main/java/model/Sprite.java
@@ -1,13 +1,40 @@
 package model;
 
-public class Sprite {
-    private char image; // a voir !
+import javax.swing.*;
+import java.awt.*;
 
-    public char getImage() {
-        return image;
+/**
+ * A sprite representing an element in the level
+ * @author Marie
+ */
+public class Sprite {
+
+    /** The image of the sprite */
+    private Image image;
+
+    /**
+     * Instanciate the sprite
+     * @param image
+     * A path to the image
+     */
+    public Sprite(String image) {
+        this.image = new ImageIcon(image).getImage();
     }
 
-    public void setImage(char image) {
+    /**
+     * Instanciate the sprite
+     * @param image
+     * The image
+     */
+    public Sprite(Image image) {
         this.image = image;
+    }
+
+    /**
+     * Get the current image of the sprite
+     * @return
+     */
+    public Image getImage() {
+        return image;
     }
 }
