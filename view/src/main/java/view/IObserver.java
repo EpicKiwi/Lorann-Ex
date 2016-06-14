e02ab808326ad4f8ebc0510e85cf2364ec238d15
clementchabrier <clement.chabrier@viacesi.fr>
2016-06-14 15:11:58 +0200

    baptiste le sac a *****

---
+++ view/src/main/java/view/IObserver.java
@@ -0,0 +1,14 @@
+package view;
+
+/**
+ * 
+ * @author Clement Chabrier
+ *interface used to build an observer patern
+ */
+public interface IObserver 
+{
+	/**
+	 * methode used to update information sent to a class
+	 */
+	public void update();
+}
