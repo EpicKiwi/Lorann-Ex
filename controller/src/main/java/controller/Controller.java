40e16ac93d7de256ac118d254421d268acf8a874
EpicKiwi <lelycandu76@gmail.com>
2016-06-15 10:13:34 +0200

    Perform order

--- controller/src/main/java/controller/Controller.java
+++ controller/src/main/java/controller/Controller.java
@@ -47,7 +47,19 @@ public class Controller implements IController,Observer {
 	 * The order to perform
      */
 	public void orderPerform(Order order) {
-
+		switch (order){
+			case CHARACTER_DOWN:
+				break;
+			case CHARACTER_UP:
+				break;
+			case CHARACTER_LEFT:
+				break;
+			case CHARACTER_RIGHT:
+				break;
+			default:
+				System.out.println("Not supported order : "+order.toString());
+				break;
+		}
 	}
 
 	/**
